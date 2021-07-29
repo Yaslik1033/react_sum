969896eefeeb7d95b926033f9910818407f85cb9
Yaslik1033 <55955258+Yaslik1033@users.noreply.github.com>
2021-07-29 14:44:09 +0300

    Updates

---
+++ precache-manifest.81ae6aad74c6050f29450faa6ccd0567.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80d5b09c1b04e6a92db6f28651fd7b64",
+    "url": "./index.html"
+  },
+  {
+    "revision": "fb15abc0ac2885192776",
+    "url": "./static/css/main.819e4dd6.chunk.css"
+  },
+  {
+    "revision": "9cb276670027b79266c7",
+    "url": "./static/js/2.bafbf7af.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.bafbf7af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb15abc0ac2885192776",
+    "url": "./static/js/main.6b841036.chunk.js"
+  },
+  {
+    "revision": "c2acbe53827daed63a5f",
+    "url": "./static/js/runtime-main.1aec85e4.js"
+  }
+]);
